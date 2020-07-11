08b97978938df1e2e894c5a3bc85034ba7489dc5
Andrew Dimmer <contactandrewd@gmail.com>
2020-07-11 14:32:07 -0400

    chore(functions): Defined Data Types

---
+++ functions/src/@types/index.d.ts
@@ -0,0 +1,30 @@
+declare interface ScoreObject extends StudentData {
+  score: number;
+  status: number;
+}
+
+declare interface StudentData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  classLevel: string;
+  bio: string;
+  questions: QAData[];
+  classes: ClassData[];
+  dorm: DormData;
+}
+
+declare interface QAData {
+  questionNumber: string;
+  answerIndex: string;
+}
+
+declare interface ClassData {
+  dept: string;
+  number: number;
+}
+
+declare interface DormData {
+  dorm: string;
+  floor?: number;
+}
