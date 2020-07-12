919a8fa1b771f4c727e9774f0dcebfe07d888ecb
Nathan Dimmer <nathandimmer@gmail.com>
2020-07-11 22:23:27 -0400

    feat(flutter): added users page

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -2,11 +2,14 @@ import 'package:classmate_connector/Routes/Home.dart';
 import 'package:classmate_connector/Routes/Questions.dart';
 import 'package:flutter/material.dart';
 
+import 'Routes/Users.dart';
+
 void main() {
   runApp(MaterialApp(
     home: Questions(), // becomes the route named '/'
     routes: <String, WidgetBuilder>{
       '/Home': (BuildContext context) => Home(),
+      '/Users': (BuildContext context) => Users(),
     },
     title: "Classmate Connector",
     theme: ThemeData(
