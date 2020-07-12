905ad4dd3603d5425b985828a8ac820a078ca46d
Andrew Dimmer <contactandrewd@gmail.com>
2020-07-11 20:44:00 -0400

    fix(flutter): Created DormObject Data Type

--- flutter/lib/Classes/BioObject.dart
+++ flutter/lib/Classes/BioObject.dart
@@ -1,20 +1,21 @@
 import 'package:classmate_connect/Classes/ClassObject.dart';
+import 'package:classmate_connect/Classes/DormObject.dart';
 
 class BioObject {
   String firstName;
   String lastName;
+  String email;
   String bio;
   List<ClassObject> classes;
-  String dorm;
-  String floor;
+  DormObject dorm;
   String classLevel;
 
   BioObject(
       {this.firstName,
       this.lastName,
+      this.email,
       this.bio,
       this.classes,
       this.dorm,
-      this.floor,
       this.classLevel});
 }
