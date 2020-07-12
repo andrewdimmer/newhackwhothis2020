e482ae349a312dc445249c351fcf4f9a7cb7fb8f
Andrew Dimmer <contactandrewd@gmail.com>
2020-07-11 20:50:32 -0400

    refactor(flutter): Streamlined Questions Route State

--- flutter/lib/Routes/Questions.dart
+++ flutter/lib/Routes/Questions.dart
@@ -1,11 +1,11 @@
 import 'package:classmate_connect/Classes/BioObject.dart';
-import 'package:classmate_connect/Classes/ClassObject.dart';
+import 'package:classmate_connect/Classes/DormObject.dart';
 import 'package:classmate_connect/Classes/QAObject.dart';
 import 'package:classmate_connect/Pages/Bio.dart';
+import 'package:classmate_connect/Pages/Email.dart';
 import 'package:classmate_connect/Pages/Password.dart';
 import 'package:classmate_connect/Pages/QuestionList.dart';
 import 'package:flutter/material.dart';
-import 'package:classmate_connect/Pages/Email.dart';
 
 class Questions extends StatefulWidget {
   Questions({
@@ -20,15 +20,14 @@ enum Stage { email, password, bio, questionList, home }
 
 class _QuestionsState extends State<Questions> {
   Stage stage = Stage.email;
-  String email = "";
-  BioObject userInfo;
-  List<Map<String, num>> dorms;
-
-  List<bool> classLevelSelected = [false, false, false, false];
-
-  String dorm = "";
-
-  String classLevel = "";
+  BioObject userInfo = BioObject(
+      firstName: "",
+      lastName: "",
+      email: "",
+      bio: "",
+      classes: [],
+      classLevel: "",
+      dorm: DormObject(dorm: "Select a Dorm"));
 
   final List<QAObject> questions = [
     QAObject(
@@ -52,39 +51,6 @@ class _QuestionsState extends State<Questions> {
   }
 
   void setClassLevelSelected(int index) {
-    if (classLevelSelected[index]) {
-      setState(() {
-        classLevelSelected[index] = false;
-        classLevel = "";
-      });
-    } else {
-      for (int i = 0; i < classLevelSelected.length; i++) {
-        setState(() {
-          classLevelSelected[i] = false;
-        });
-      }
-      classLevelSelected[index] = true;
-      if (index == 0) {
-        setState(() {
-          classLevel = "Freshman";
-        });
-      } else if (index == 1) {
-        setState(() {
-          classLevel = "Sophomore";
-        });
-      } else if (index == 2) {
-        setState(() {
-          classLevel = "Junior";
-        });
-      } else if (index == 3) {
-        setState(() {
-          classLevel = "Senior";
-        });
-      }
-    }
-  }
-
-  void setStage(Stage newStage) {
     setState(() {
       stage = newStage;
     });
@@ -92,34 +58,12 @@ class _QuestionsState extends State<Questions> {
 
   void setEmail(String newEmail) {
     setState(() {
-      email = newEmail;
+      userInfo.email = newEmail;
     });
   }
 
-  void setUserInfo(String firstName, String lastName, String bio,
-      List<ClassObject> classes, String dorm, String floor, String classLevel) {
-    setState(() {
-      userInfo = BioObject(
-          firstName: firstName,
-          lastName: lastName,
-          bio: bio,
-          classes: classes,
-          dorm: dorm,
-          floor: floor,
-          classLevel: classLevel);
-    });
-  }
-
-  void setAnswer(int index, String answer) {
-    setState(() {
-      questions[index].answer = index;
-    });
-  }
-
-  void setDorm(String dormName) {
-    setState(() {
-      dorm = dormName;
-    });
+  void _updateState() {
+    setState(() {});
   }
 
   @override
