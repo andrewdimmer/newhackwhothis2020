5fd5766f2a8415d005fb926d96d184ca37ae30f5
Nathan Dimmer <nathandimmer@gmail.com>
2020-07-11 23:10:08 -0400

    feat(data): compiled questions (#12)

---
+++ flutter/lib/Data/Data.dart
@@ -0,0 +1,112 @@
+import 'dart:html';
+import 'package:classmate_connector/Classes/QAObject.dart';
+import 'package:flutter/material.dart';
+
+List<QAObject> questions = [
+  QAObject(
+      question: "How do you have the most fun?",
+      answers: ["Alone", "With a few friends", "With a large group"]),
+  QAObject(
+      question: "Are you a competitive person?",
+      answers: ["Yes", "No", "I don't know"]),
+  QAObject(
+      question: "How do you like music to be played?",
+      answers: ["Soft", "A little loud", "Very loud"]),
+  QAObject(
+      question: "You can't live without:",
+      answers: ["Music", "Social Media", "Movies / TV", "None of the above"]),
+  QAObject(
+      question: "At a movie, you would pay the most attention to:",
+      answers: ["Friends", "The movie", "The popcorn", "Your date"]),
+  QAObject(question: "Going out to eat at a nice resturant:", answers: [
+    "You enjoy several times a year",
+    "Is for special occasions only",
+    "Is a waste of money"
+  ]),
+  QAObject(question: "At a dance, you prefer to:", answers: [
+    "Dance most dances",
+    "Dance fast",
+    "Dance slow",
+    "Listen to the music and talk with friends",
+    "Don't go in the first place"
+  ]),
+  QAObject(question: "You would most like to vacation:", answers: [
+    "On a beach",
+    "In a major city",
+    "In the mountains",
+    "In a foreign country"
+  ]),
+  QAObject(question: "What are most of your decisions based off of?", answers: [
+    "Logic",
+    "Intuition, what feels right",
+    "Friends advice",
+    "Guesses"
+  ]),
+  QAObject(question: "How much emphasis do you put on school?", answers: [
+    "It's very important to you",
+    "A moderate ammount",
+    "Little",
+    "None"
+  ]),
+  QAObject(question: "Money should be:", answers: [
+    "Spent on the necessities of life - the rest should be saved",
+    "Spent on the necessities of life and occasionally buying material items",
+    "Spent having a good time and enjoying life"
+  ]),
+  QAObject(
+      question: "Where is your favorite place to chill out?",
+      answers: ["Home", "Sports events", "Mall", "Movies", "Friend's house"]),
+  QAObject(question: "What is your favorite music genre", answers: [
+    "Country",
+    "Rock",
+    "R&B",
+    "Pop",
+    "Alternative",
+    "Instrumental",
+    "Jazz",
+    "Soul",
+    "K-Pop"
+  ]),
+  QAObject(question: "What's your favorite sport?", answers: [
+    "Football",
+    "Baseball",
+    "Basketball",
+    "Hockey",
+    "Soccer",
+    "Tennis",
+    "Swim",
+    "Running",
+    "Biking",
+    "Skiing / Snowboarding",
+    "Other",
+    "I don't like sports"
+  ]),
+  QAObject(question: "What's your favorite food?", answers: [
+    "Pizza",
+    "Bacon",
+    "Steak",
+    "Pasta",
+    "Ice Cream",
+    "Chicken",
+    "Sushi",
+    "Grilled Cheese",
+    "Cheeseburger",
+    "Pancakes",
+    "Other"
+  ]),
+  QAObject(question: "What's your favorite color?", answers: [
+    "Red",
+    "Orange",
+    "Yellow",
+    "Green",
+    "Blue",
+    "Indigo",
+    "Purple",
+    "White",
+    "Brown",
+    "Black",
+    "Pink",
+    "Other"
+  ]),
+  QAObject(question: "Which are you?", answers: ["Night owl", "Early bird"])
+];
